4af8056ab93f64a941b5f8fac9b0d6d170676d6c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-29 23:34:10 +0000

    Implement raylib ellipses

---
+++ raylib/ellipses/main.c
@@ -0,0 +1,33 @@
+#include <raylib.h>
+
+int main(int argc, char **argv) {
+    const int screenWidth = 1024;
+    const int screenHeight = 768;
+    const char *title = "Ellises";
+    const int midX = screenWidth / 2;
+    const int midY = screenHeight / 2;
+
+   SetConfigFlags(FLAG_VSYNC_HINT);
+    InitWindow(screenWidth, screenHeight, title);
+    SetTargetFPS(60);
+
+    int endRadius = 1;
+    int step = 10;
+
+    while (!WindowShouldClose()) {
+        endRadius += step;
+        if (endRadius > 350) endRadius = 350;
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        for (int r = 1; r <= endRadius; r += step) {
+            DrawEllipseLines(midX, midY, r, 350 - r, RED);
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
