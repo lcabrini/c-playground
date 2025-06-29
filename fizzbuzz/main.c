db40bfa87483952c73b8cb519c13cd3dbb0dfbbc
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-29 23:12:58 +0000

    Minor update

--- fizzbuzz/main.c
+++ fizzbuzz/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv) {
     for (int i = 1; i <= 100; ++i) {
-        if (i % 15 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
             printf("fizz buzz\n");
         } else if (i % 3 == 0) {
             printf("fizz\n");
